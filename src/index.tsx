72918b6603fdc6b3fd6cc7563c30b4698a926a87
Eddi Fernando Morera Hernandez <efmorerah@gmail.com>
2021-12-06 14:52:26 -0500

    Ajustes Rutas proyecto

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Footer, Header } from '../components';
-import HomePage from '../pages/HomePage';
-import styles from './Navigation.module.scss';
+import { Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Footer, Header } from "../components";
+import HomePage from "../pages/HomePage";
+import styles from "./Navigation.module.scss";
 
 const Navigation: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <div className={styles.main}>
-                <Header />
-                <Routes>
-                    <Route element={<HomePage />} path="/home" />
-                    <Route element={<HomePage />} path="*" />
-                </Routes>
-                <Footer />
-            </div>
-        </BrowserRouter>
-    )
-}
+  return (
+    <Suspense fallback={null}>
+      <div className={styles.main}>
+        <Header />
+        <Routes>
+          <Route element={<HomePage />} path="/home" />
+          <Route element={<Navigate replace to="/home" />} path="/*" />
+        </Routes>
+        <Footer />
+      </div>
+    </Suspense>
+  );
+};
 
 export default Navigation;
